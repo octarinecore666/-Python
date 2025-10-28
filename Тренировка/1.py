905ad56d23b0a97c845d024e8aa6b9f6c2f60a4f
octarinecore666 <xanaxgirl878@gmail.com>
2025-10-28 17:20:53 +0700

    Описание изменений

---
+++ Тренировка/1.py
@@ -0,0 +1,29 @@
+# Мой рост
+my_heigh = 167
+print(my_heigh)
+
+# Мое имя
+my_name = "Юлия"
+my_name = "Юлия Косарева"
+print(my_name)
+
+# имя питомца
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!")
+print_python()
+
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
